a380ae1e6e47f282d328f3b5d36290ee82cec1cd
FeliOdras <ariane.feldmann@odras.de>
2019-02-06 16:01:35 +0100

    Added sort by artist function

--- assets/main.js
+++ assets/main.js
@@ -4,37 +4,39 @@ class TrackList {
   // Creating our Class
   constructor(domSelector, data) {
     // Getting a domelement
-    this.container = document.querySelector(domSelector)
+    this.container = document.querySelector(domSelector);
     // Store my data
-    this.data = data
+    this.data = data;
     // Represents the currently displayed data
-    this.viewData = data
-
-
-    // sort by price
-    this.sortPricing()
+    this.viewData = data;
+    this.sortArtist();
     // Show stuff
-    this.render()
+    this.render();
   }
 
   modViewData(newData) {
-    this.viewData = newData
-    this.render()
+    this.viewData = newData;
+    this.render();
   }
 
   template(music) {
     // Mapping over data and returning HTML String
-    const musicList = music.map(track =>
-      `
+    const musicList = music
+      .map(
+        track =>
+          `
         <section class="row">
-          <div class="trackArtwork"><img src="${track.artworkUrl100}" alt="${track.trackName}"></div>
+          <div class="trackArtwork"><img src="${track.artworkUrl100}" alt="${
+            track.trackName
+          }"></div>
           <div class="trackTitle"> ${track.trackName} </div>
           <div class="artistName">${track.artistName} </div>
           <div class="trackPrice">${track.trackPrice} </div>
         </section>
       `
-    ).join("")
-    return musicList
+      )
+      .join("");
+    return musicList;
 
     // For now we just assume that all data is there and that it is
     // from datatype string
@@ -43,24 +45,30 @@ class TrackList {
 
   sortPricing() {
     // TODO: Create a Methode to sort by pricing
-    music.sort((a, b) => a.trackPrice - b.trackPrice)
+    music.sort((a, b) => a.trackPrice - b.trackPrice);
+  }
+
+  sortArtist() {
+    music.sort((a, b) =>
+      a.artistName > b.artistName ? 1 : a.artistName < b.artistName ? -1 : 0
+    );
   }
 
   render() {
     // Out put will hold the complete view
-    let output = ""
+    let output = "";
 
     // Setting up data for our view
-    const header = "<h1>My Tracks</h1>"
+    const header = "<h1>My Tracks</h1>";
     // template methode accepts data to view and returns html string
-    const template = this.template(this.viewData)
+    const template = this.template(this.viewData);
     // Adding data in to our view !Order Matters!
-    output += header
-    output += "<p>Data from iTunes</p>"
-    output += template
+    output += header;
+    output += "<p>Data from iTunes</p>";
+    output += template;
     // Assinging view in to innerHTML of our domElement form the constructor
-    this.container.innerHTML = output
+    this.container.innerHTML = output;
   }
 }
 
-const myTrackList = new TrackList("#tracks", music)
+const myTrackList = new TrackList("#tracks", music);
